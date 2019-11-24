c36c2b972da07e8284c867375bc9bd07645b4d8d
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2019-11-24 22:33:56 +0200

    add page with registration form for the next meetup

--- run_local_dev.sh
+++ run_local_dev.sh
@@ -3,7 +3,7 @@
 JEKYLL_VERSION=4.0
 CONTAINER_NAME=jekyll-local-dev
 
-if [ ! "$(docker ps -a -f name=$CONTAINER_NAME)" ]; 
+if [ ! "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; 
 then
   docker run -it -p4000:4000 --volume="$PWD:/srv/jekyll" --name "$CONTAINER_NAME" jekyll/jekyll:$JEKYLL_VERSION jekyll serve
 else
